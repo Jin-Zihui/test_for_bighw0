4ff308fa03c475358bb399150424d6d85d7d400e
Jin-Zihui <54518708+Jin-Zihui@users.noreply.github.com>
2021-01-17 12:26:15 +0800

    Update Tokenizer.java

--- c0/src/tokenizer/Tokenizer.java
+++ c0/src/tokenizer/Tokenizer.java
@@ -158,19 +158,9 @@ public class QuickSort {
 	
 	public static void main(String[] args) {
 		int[] num = {3,45,78,64,52,11,64,55,99,11,18};
-		System.out.println(arrayToString(num,"未排序"));
 		QuickSort(num,0,num.length-1);
-		System.out.println(arrayToString(num,"排序"));
-		System.out.println("数组个数："+num.length);
-		System.out.println("循环次数："+count);
 		
 	}
-	/**
-	 * 快速排序
-	 * @param num	排序的数组
-	 * @param left	数组的前针
-	 * @param right 数组后针
-	 */
 	private static void QuickSort(int[] num, int left, int right) {
 		//如果left等于right，即数组只有一个元素，直接返回
 		if(left>=right) {
@@ -203,12 +193,6 @@ public class QuickSort {
 		QuickSort(num,left,i-1);
 		QuickSort(num,i+1,right);
 	}
-	/**
-	 * 将一个int类型数组转化为字符串
-	 * @param arr
-	 * @param flag
-	 * @return
-	 */
 	private static String arrayToString(int[] arr,String flag) {
 		String str = "数组为("+flag+")：";
 		for(int a : arr) {
