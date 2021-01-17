167310dd71213d20d3cf9e428c30c54fdf8dac4e
Jin-Zihui <54518708+Jin-Zihui@users.noreply.github.com>
2021-01-17 12:27:24 +0800

    Update Tokenizer.java

--- c0/src/tokenizer/Tokenizer.java
+++ c0/src/tokenizer/Tokenizer.java
@@ -153,46 +153,6 @@ public class Tokenizer {
         throw new TokenizeError(ErrorCode.InvalidInput, it.previousPos());
     }
     
-public class QuickSort {
-	private static int count;
-	
-	public static void main(String[] args) {
-		int[] num = {3,45,78,64,52,11,64,55,99,11,18};
-		QuickSort(num,0,num.length-1);
-		
-	}
-	private static void QuickSort(int[] num, int left, int right) {
-		//如果left等于right，即数组只有一个元素，直接返回
-		if(left>=right) {
-			return;
-		}
-		//设置最左边的元素为基准值
-		int key=num[left];
-		//数组中比key小的放在左边，比key大的放在右边，key值下标为i
-		int i=left;
-		int j=right;
-		while(i<j){
-			//j向左移，直到遇到比key小的值
-			while(num[j]>=key && i<j){
-				j--;
-			}
-			//i向右移，直到遇到比key大的值
-			while(num[i]<=key && i<j){
-				i++;
-			}
-			//i和j指向的元素交换
-			if(i<j){
-				int temp=num[i];
-				num[i]=num[j];
-				num[j]=temp;
-			}
-		}
-		num[left]=num[i];
-		num[i]=key;
-		count++;
-		QuickSort(num,left,i-1);
-		QuickSort(num,i+1,right);
-	}
 	private static String arrayToString(int[] arr,String flag) {
 		String str = "数组为("+flag+")：";
 		for(int a : arr) {
